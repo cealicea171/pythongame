f678a013243297dc75cc9ef64809a6ac4558a1e0
Calista Alicea <cealicea171@stevenscollege.edu>
2019-02-05 01:36:03 -0500

    create some classes with code

--- engine.py
+++ engine.py
@@ -1,1 +1,7 @@
-...
+class Engine(object):
+
+    def __init__(self, scene_map):
+        pass
+
+    def play(self):
+        pass
---
+++ map.py
@@ -0,0 +1,22 @@
+ class Scene(object):
+
+     def enter(self):
+         pass
+
+
+ class Death(Scene):
+
+     def enter(Self):
+         pass
+
+
+class Map(object):
+
+    def __init__(self, start_game):
+        pass
+
+    def next_scene(self, scene_name):
+        pass
+
+    def opening_scene(self):
+        pass
